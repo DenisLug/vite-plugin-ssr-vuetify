4e7711869b62605173066ab145c45741bb16c536
Denis Lugowski <denis.lugowski@tchibo.de>
2022-07-25 15:25:23 +0200

    Remove import of components and directives for vuetify

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,25 +1,4 @@
 import '../styles/main.scss'
 import { createVuetify } from 'vuetify'
-// @ts-ignore
-import * as components from 'vuetify/lib/components/index.mjs'
-// @ts-ignore
-import * as directives from 'vuetify/lib/directives/index.mjs'
-// @ts-ignore
-import colors from 'vuetify/lib/util/colors.mjs'
 
-export default createVuetify({
-  components,
-  directives,
-  theme: {
-    themes: {
-      light: {
-        colors: {
-          footerColor: '#C0A468',
-          background: colors.grey.lighten5
-        },
-        dark: false,
-        variables: {}
-      }
-    }
-  }
-})
+export default createVuetify()
